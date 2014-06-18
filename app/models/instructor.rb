2c730271098b11066d717f95179e2d959218c9e1
stiven sanchez <salasvelez03@gmail.com>
2014-06-18 11:54:02 -0500

    este es la cuarta parte

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,5 +1,22 @@
 class Instructor < ActiveRecord::Base
-  belongs_to :tipodocumento
+
+	# Validar que los atributos sean obligatorios
+	validates :tipodocumento, :presence => true
+	validates :numedoc, :presence => true
+	validates :nombre, :presence => true
+	validates :apellido, :presence => true
+	validates :area, :presence => true
+	validates :rol, :presence => true
+	validates :correo, :presence => true
+	# validar un atributo booleano
+	# validates :genero, inclusion: [true, false]
+
+	# validar que un atributos unicos
+    validates :numedoc, uniqueness: { message: "No se puede Repetir" }
+    validates :correo, uniqueness: { message: "No se puede Repetir" }
+
+
+    belongs_to :tipodocumento
   def self.search(search, page)
    where(['upper(nombre) like ?',
    "%#{search}%".upcase]).paginate(page: page, per_page: 5).order("nombre")
--- app/models/operacio.rb
+++ app/models/operacio.rb
@@ -1,4 +1,14 @@
 class Operacio < ActiveRecord::Base
+
+	# Validar que los atributos sean obligatorios
+	validates :area, :presence => true
+	validates :cantidadProceso, :presence => true
+	validates :maquinas, :presence => true
+	validates :instructorCargo, :presence => true
+	validates :insumos, :presence => true
+	validates :Referencia, :presence => true
+	validates :Descripcion, :presence => true
+	
 	def self.search(search, page)
      where(['upper(area) like ?',
      "%#{search}%".upcase]).paginate(page: page, per_page: 1).order("area")
